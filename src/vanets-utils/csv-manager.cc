1d3b0cc2ea0f4ef7cd7947a6cc1ab1bf03b8f1e8
Nicola Ursino <nicola.ursino@gmail.com>
2025-05-11 22:15:01 +0200

    Moved CSVManager into the vanets-utils module. Now FBVanetExperiment and ROFFVanetExperiment can share the same code

---
+++ src/vanets-utils/csv-manager.cc
@@ -0,0 +1,174 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2025 University of Padova
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "csv-manager.h"
+#include "ns3/log.h"
+#include <sys/time.h>
+#include <iostream>
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("CSVManager");
+
+CSVManager::CSVManager ()
+  : m_csvFilePath (""),
+    runId(0)
+{
+  NS_LOG_FUNCTION (this);
+}
+
+CSVManager::CSVManager(unsigned int runId)
+  : m_csvFilePath(""),
+    runId(runId)
+{
+  NS_LOG_FUNCTION(this);
+}
+
+CSVManager::~CSVManager ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+CSVManager::Setup (std::string filename)
+{
+  NS_LOG_FUNCTION (this);
+  m_csvFilePath = filename;
+}
+
+void
+CSVManager::WriteHeader (std::string header)
+{
+  NS_LOG_FUNCTION (this);
+
+  boost::filesystem::path parentPath = m_csvFilePath.parent_path();
+  boost::filesystem::create_directories(parentPath);
+  std::ofstream out (m_csvFilePath.string());
+  out << header.c_str() << std::endl;
+  out.close ();
+}
+
+std::string
+CSVManager::GenerateRandomTag()
+{
+  static const char charset[] =
+    "abcdefghijklmnopqrstuvwxyz"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "0123456789";
+  std::default_random_engine rng(std::random_device{}());
+  std::uniform_int_distribution<> dist(0, sizeof(charset) - 2);
+  std::string tag;
+  for (int i = 0; i < 4; ++i) {
+    tag += charset[dist(rng)];
+  }
+  return tag;
+}
+
+void
+CSVManager::EnableAlternativeFilename(boost::filesystem::path path)
+{
+  NS_LOG_FUNCTION (this);
+  std::string extension = ".csv";
+
+  // Get unix time
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+
+  // Create the new filename
+  std::string finalPartOfPath = "-" + std::to_string(ms) + "-" + GenerateRandomTag() + extension;
+  m_csvFilePath = path;
+  m_csvFilePath += finalPartOfPath;
+  std::cout << "CSV File Path = " << m_csvFilePath << std::endl;
+}
+
+void
+CSVManager::AddValue(std::stringstream value)
+{
+  NS_LOG_FUNCTION (this);
+  m_currentRow << value.str() << ",";
+}
+
+void
+CSVManager::AddValue(int value)
+{
+  NS_LOG_FUNCTION (this);
+  m_currentRow << value << ",";
+}
+
+void
+CSVManager::AddValue(double value)
+{
+  NS_LOG_FUNCTION (this);
+  m_currentRow << value << ",";
+}
+
+void
+CSVManager::AddValue(std::string value)
+{
+  NS_LOG_FUNCTION (this);
+  m_currentRow << value << ",";
+}
+
+void
+CSVManager::AddMultipleValues(std::stringstream& value)
+{
+  NS_LOG_FUNCTION (this);
+  m_currentRow << value.str();
+}
+
+void
+CSVManager::SetRunId(unsigned int id)
+{
+  NS_LOG_FUNCTION(this << id);
+  runId = id;
+}
+
+void
+CSVManager::CloseRow (void)
+{
+  NS_LOG_FUNCTION (this);
+  // write current row to file in an atomic way
+  std::ofstream out (m_csvFilePath.c_str (), std::ios::app);
+  out << runId << "," << m_currentRow.str() << std::endl;
+  out.close ();
+  // Delete (old) row
+  m_currentRow.str("");
+}
+
+bool
+CSVManager::IsFileEmptyOrNonexistent(const boost::filesystem::path& path)
+{
+  NS_LOG_FUNCTION(path.string());
+
+  // Check if the file exists
+  if (!boost::filesystem::exists(path)) {
+    NS_LOG_INFO("File does not exist: " << path.string());
+    return true;
+  }
+
+  // Check if the file is empty
+  if (boost::filesystem::file_size(path) == 0) {
+    NS_LOG_INFO("File is empty: " << path.string());
+    return true;
+  }
+
+  return false;
+}
+
+} // namespace ns3
---
+++ src/vanets-utils/csv-manager.h
@@ -0,0 +1,151 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2025 University of Padova
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef CSV_MANAGER_H
+#define CSV_MANAGER_H
+
+#include "ns3/core-module.h"
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include <random>
+
+namespace ns3 {
+
+/**
+ * \ingroup vanets-utils
+ * \brief A utility class for managing CSV files
+ *
+ * CSVManager helps with creating and writing to CSV files.
+ * It provides methods for setting up the file, writing headers,
+ * adding values to rows, and closing rows.
+ */
+class CSVManager
+{
+public:
+  /**
+   * \brief Default constructor
+   * \return none
+   */
+  CSVManager ();
+
+  /**
+   * \brief Constructor with runId
+   * \param runId ID of the current run
+   */
+  CSVManager(unsigned int runId);
+
+  /**
+   * \brief Destructor
+   * \return none
+   */
+  ~CSVManager ();
+
+  /**
+   * \brief Set the filename of the csv file
+   * \param filename path of the file
+   * \return none
+   */
+  void Setup (std::string filename);
+
+  /**
+   * \brief Write the header of the csv
+   * \param header header of the csv
+   * \return none
+   */
+  void WriteHeader(std::string header);
+
+  /**
+   * \brief generates a random string to append to the csv filename
+   * \return a random string of 4 characters
+   */
+  std::string GenerateRandomTag();
+
+  /**
+   * \brief Create a new filename adding a timestamp to a provided base
+   * \param path The path to use as base for the alternative filename
+   * \return none
+   */
+  void EnableAlternativeFilename(boost::filesystem::path path);
+
+  /**
+   * \brief Add a value (cell) in the current row
+   * \param value int value to be written
+   * \return none
+   */
+  void AddValue(int value);
+
+  /**
+   * \brief Add a value (cell) in the current row
+   * \param value double value to be written
+   * \return none
+   */
+  void AddValue(double value);
+
+  /**
+   * \brief Add a value (cell) in the current row
+   * \param value string value to be written
+   * \return none
+   */
+  void AddValue(std::string value);
+
+  /**
+   * \brief Add a value (cell) in the current row
+   * \param value stream value to be written
+   * \return none
+   */
+  void AddValue(std::stringstream value);
+
+  /**
+   * \brief Add multiple values in the current row
+   * \param value stream to be written
+   * \return none
+   */
+  void AddMultipleValues(std::stringstream& value);
+
+  /**
+   * \brief Set the run ID manually
+   * \param id The run ID to set
+   * \return none
+   */
+  void SetRunId(unsigned int id);
+
+  /**
+   * \brief Write the current row and initialize a new one
+   * \return none
+   */
+  void CloseRow(void);
+
+  /**
+   * \brief Check if a file is empty or doesn't exist
+   *
+   * \param path The Boost path to check.
+   * \return true if the file doesn't exist or is empty.
+   */
+  static bool IsFileEmptyOrNonexistent(const boost::filesystem::path& path);
+
+private:
+  boost::filesystem::path m_csvFilePath;   //!< Path to the CSV file
+  std::stringstream m_currentRow;          //!< Current row being built
+  unsigned int runId;                      //!< ID of the current run
+};
+
+} // namespace ns3
+
+#endif /* CSV_MANAGER_H */
